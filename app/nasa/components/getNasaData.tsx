fb3974fc872164e40c7be722eb488adb59ddb0e4
dansever <dansever96@gmail.com>
2024-11-16 17:42:31 +0200

    test

--- app/nasa/components/getNasaData.tsx
+++ app/nasa/components/getNasaData.tsx
@@ -2,7 +2,7 @@
 
 export async function getData(count: number) {
     const apiKey = process.env.NEXT_PUBLIC_NASA_API_KEY;
-    
+    console.log({apiKey})
     if (!apiKey) {
       throw new Error("NASA API key is not defined in environment variables.");
     }
