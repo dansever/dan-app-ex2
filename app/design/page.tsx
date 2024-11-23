120003e0eeef401c81c28d8976eab46d47cb6f48
dansever <dansever96@gmail.com>
2024-11-23 23:05:27 +0200

    design from Figma

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -1,9 +1,37 @@
 import styles from "./page.module.css";
 
 export default function Demos() {
+
   return (
-    <main>
-      <h1 className={styles.hey}>Hey</h1>
+    <main className={styles.main}>
+      <img
+        className={styles.logo}
+        src="/sock_time_logo.png" 
+        alt="Sock Time Logo" 
+      />
+
+      <div className={styles.mainContainer}>
+
+        <div className={styles.textBox}>
+          <h2>Sock Showoff</h2>  
+          <h3>Share what’s under the table - your socks! Enter your username and password to begin.</h3>
+        </div>
+
+        <form className={styles.form}>
+          <input
+            className={styles.input} type="email" id="email" name="email" placeholder="Enter your email"
+            required
+          />
+          <div className={styles.inputGroup}>
+            <input 
+              className={styles.input} type="password" id="password" name="password" placeholder="password"
+              required
+            />
+            <p>Forgot password?</p>
+          </div>
+          <button className={styles.submitButton} type="submit"> Let's Go!</button>
+        </form>        
+      </div>
     </main>
   );
 }
